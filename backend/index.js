fefe335c8cb46df964bb90421b01a5c0eabec0ed
Alexander200330 <alexanderventura0630@gmail.com>
2023-07-11 22:50:01 -0400

    Conexión base de datos Mongodb y agregar url en variables de entorno

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import conectarDB from "./config/db.js";
+import dotenv from "dotenv";
 
 const app = express();
 
-app.listen(4000, () => {
-    console.log("Corriendo servidor en el puerto 4000");
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`Corriendo servidor en el puerto ${PORT}`);
 })
